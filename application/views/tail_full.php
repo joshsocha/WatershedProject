9f94557892a90ad34f5922f756dd4b1c1e58e412
greg-qds <greg.qds@gmail.com>
2013-09-14 11:52:56 -0400

    added phantasm/joyride and fixed what i broke ;)

--- application/views/tail_full.php
+++ application/views/tail_full.php
@@ -1,5 +1,6 @@
 </div>
 <script type="text/javascript" src="<?php echo base_url(); ?>/application/assets/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>/application/assets/js/bootstrap.min.js"></script>
+<?php /* TODO: Load JS files from $data['extra_js'] */ ?>
 </body>
 </html>
