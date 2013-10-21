f8bde7221ee9e122e61c95f9bb5c053f83ea2dd3
greg-qds <greg.qds@gmail.com>
2013-10-21 14:08:01 -0400

    started adding help page

---
+++ bonfire/modules/help/language/english/help_lang.php
@@ -0,0 +1,30 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$lang['help_manage']			= 'Manage Help';
+$lang['help_edit']				= 'Edit';
+$lang['help_true']				= 'True';
+$lang['help_false']				= 'False';
+$lang['help_create']			= 'Create';
+$lang['help_list']				= 'List';
+$lang['help_new']				= 'New';
+$lang['help_edit_text']			= 'Edit this to suit your needs';
+$lang['help_no_records']			= 'There aren\'t any help in the system.';
+$lang['help_create_new']			= 'Create a new Help.';
+$lang['help_create_success']			= 'Help successfully created.';
+$lang['help_create_failure']			= 'There was a problem creating the help: ';
+$lang['help_create_new_button']			= 'Create New Help';
+$lang['help_invalid_id']			= 'Invalid Help ID.';
+$lang['help_edit_success']			= 'Help successfully saved.';
+$lang['help_edit_failure']			= 'There was a problem saving the help: ';
+$lang['help_delete_success']			= 'record(s) successfully deleted.';
+$lang['help_delete_failure']			= 'We could not delete the record: ';
+$lang['help_delete_error']			= 'You have not selected any records to delete.';
+$lang['help_actions']			= 'Actions';
+$lang['help_cancel']			= 'Cancel';
+$lang['help_delete_record']			= 'Delete this Help';
+$lang['help_delete_confirm']			= 'Are you sure you want to delete this help?';
+$lang['help_edit_heading']			= 'Edit Help';
+
+// Activities
+$lang['help_act_create_record']			= 'Created record with ID';
+$lang['help_act_edit_record']			= 'Updated record with ID';
+$lang['help_act_delete_record']			= 'Deleted record with ID';
