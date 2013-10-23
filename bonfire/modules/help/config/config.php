eee9b2f84e606257f9f61c14cb5684de39d6f288
greg-qds <greg.qds@gmail.com>
2013-10-23 14:20:24 -0400

    changed help config. premerge commit just in case

--- bonfire/modules/help/config/config.php
+++ bonfire/modules/help/config/config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');$config['module_config'] = array(
-	'description'	=> 'Your module description',
+	'description'	=> 'Help Page Module',
 	'name'		=> 'Help',
 	'version'		=> '0.0.1',
-	'author'		=> 'Admin'
+	'author'		=> 'Greg'
 );
