8f5a8b2f253f96e345847c6081e577045ac7b884
greg-qds <greg.qds@gmail.com>
2013-10-28 14:44:52 -0400

    Added sql script to add help content. Whill write up actual migration later

---
+++ bonfire/modules/help/migrations/AddHelpContent.sql
@@ -0,0 +1,4 @@
+INSERT INTO `bf_help` (`id`, `help_title`, `help_text`, `help_type`) VALUES
+(1, 'Help', 'Welcome to the help page. I you are new to the site I would suggest taking the site tour. For other questions plese see the FAQs below', 'head'),
+(2, 'How can I start reporting about watersheds near me?', 'In order to submit observations, you must be a registered user. Registration is free, just click the Sign Up link in the top right corner', 'FAQ'),
+(3, 'How do I become a trusted user', 'In order to become a trusted user  you must have 15 of your observations approved by an trusted user', 'FAQ');
