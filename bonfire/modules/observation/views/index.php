f4ff803de71eb9da8c143cd1af2a294ef9a04320
Nick Lanam <nick.lanam@tekraq.com>
2013-11-30 11:14:52 -0500

    Pagination links now account for current URL. They should have done this before, but CodeIgniter was 'fixing' relative links incorrectly.

--- bonfire/modules/observation/views/index.php
+++ bonfire/modules/observation/views/index.php
@@ -52,26 +52,7 @@
 	<?php endif; ?>
 
 	<br/>
-	<div class="pagination" style="width:100%;text-align:center;">
-		<ul>
-			<?php
-			// Previous
-			echo '<li'.($curpage == 1? ' class="disabled"' : '').'>';
-			echo '  <a href="'.($curpage-1).'"">&laquo;</a></li>';
-
-			// Show no more than 7 page links
-			$initial = max(1, $curpage-3);
-			$limit   = min($numpages+1, $initial+7);
-			for($i = $initial; $i < $limit; $i++) {
-				echo '<li'.($i==$curpage?' class="active"':'').'><a href="'.$i.'">'.$i.'</a></li>';
-			}
-
-			// Next
-			echo '<li'.($curpage == $numpages? ' class="disabled"' : '').'>';
-			echo '  <a href="'.($curpage+1).'"">&raquo;</a></li>';
-			?>
-		</ul>
-	</div>
+	<?php echo theme_view('parts/pagination'); ?>
 <?php else:
 	echo '<h3>' . lang('observation_no_reports') . '</h3>';
 	if($this->auth->has_permission('Observation.Reports.Create')) {
--- bonfire/themes/default/parts/pagination.php
+++ bonfire/themes/default/parts/pagination.php
@@ -4,18 +4,18 @@
 		<?php
 		// Previous
 		echo '<li'.($curpage == 1? ' class="disabled"' : '').'>';
-		echo '  <a href="'.max($curpage-1,1).'">&laquo;</a></li>';
+		echo '  <a href="'.current_url().'/'.max($curpage-1,1).'">&laquo;</a></li>';
 
 		// Show no more than 7 page links
 		$initial = max(1, $curpage-3);
 		$limit   = min($numpages+1, $initial+7);
 		for($i = $initial; $i < $limit; $i++) {
-			echo '<li'.($i==$curpage?' class="active"':'').'><a href="'.$i.'">'.$i.'</a></li>';
+			echo '<li'.($i==$curpage?' class="active"':'').'><a href="'.current_url().'/'.$i.'">'.$i.'</a></li>';
 		}
 
 		// Next
 		echo '<li'.($curpage >= $numpages? ' class="disabled"' : '').'>';
-		echo '  <a href="'.min($curpage+1,$numpages).'">&raquo;</a></li>';
+		echo '  <a href="'.current_url().'/'.min($curpage+1,$numpages).'">&raquo;</a></li>';
 		?>
 	</ul>
 </div>
